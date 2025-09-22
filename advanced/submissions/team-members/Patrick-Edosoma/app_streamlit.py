0b537b45d879cc900a8cca84f6e10ff3aca76950
Patrick Edosoma <157173680+PATRICK079@users.noreply.github.com>
2025-09-22 10:51:48 +0300

    Update app_streamlit.py

--- advanced/submissions/team-members/Patrick-Edosoma/app_streamlit.py
+++ advanced/submissions/team-members/Patrick-Edosoma/app_streamlit.py
@@ -8,7 +8,9 @@ from typing import List, Optional
 
 import streamlit as st
 #import load_dotenv
-
+import sys
+sys.path.append(os.path.dirname(__file__))      
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 # Optional: Gemini (google-generativeai)
 GEMINI_AVAILABLE = False
 try:
